4017b9d5111b81f79ee5f8a50ff9e113974f096a
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-01 17:05:25 +0530

    Add bloglist frontend component

---
+++ blogui/src/components/BlogList.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react'
+
+const BlogList = () => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(BASE_URL+'api/blog/posts')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setData(data)
+            setLoading(false)
+        })
+        .catch(err => {
+            setError(err.message)
+            setLoading(false)
+        })
+    }, [])
+
+    return (
+        <div style={{ margin: 10, padding: 10}}>
+            <h1>Post List</h1>
+            {error && (
+                <h4 style={{padding: 5, color: "red"}}> An error occured: {error} </h4>
+            )}
+            {loading ? <div>Loading...</div> : (
+                <div>
+                    {data && (
+                        <div>There is data</div>
+                    )}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default BlogList
