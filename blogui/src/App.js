3b6e2e888a371d2790ee4e363376b8c76968be60
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-02 17:28:14 +0530

    Restart frontend styles

--- blogui/src/App.js
+++ blogui/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import {Router, Route, Routes } from 'react-router-dom'
-import 'fomantic-ui-css/semantic.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
 import { createBrowserHistory } from 'history';
 import PostList from './container/PostList'
+import 'fomantic-ui-css/semantic.css';
 
 const history = createBrowserHistory()
+
 const App = () => { 
     return (
         <Router history={history}>
-            <Routes>
-                <Route path="/"  component={PostList} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<PostList />} />
+          </Routes>
         </Router>
     )
 }
