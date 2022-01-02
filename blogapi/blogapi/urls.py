7e3757ef1d0114b1cd8fbf7a2fc84bf254c6e766
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-02 18:51:52 +0530

    Add post model

--- blogapi/blogapi/urls.py
+++ blogapi/blogapi/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    # path('accounts/', include('allauth.urls')),
+    path('api/', include('posts.urls', namespace='posts')),
 ]
--- blogapi/posts/models.py
+++ blogapi/posts/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+def upload_post_image(instance, filename):
+    return f'{instance.user}/{filename}'
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=127)
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()    # markdown
+    thumbnail = models.ImageField(upload_to=upload_post_image)
+    def __str__(self):
+        return self.title
