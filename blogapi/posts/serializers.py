a00495fc6cc9170ed6904339513014fc17a7581a
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-02 19:07:43 +0530

    Add Post view and serializer

---
+++ blogapi/posts/serializers.py
@@ -0,0 +1,8 @@
+from django.db import models
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.utils import serializer_helpers
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostListView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny,]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
