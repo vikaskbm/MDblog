1b8dea34b8419efef2a938f19e22e89b5df47fe5
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-02 18:57:40 +0530

    Add auto slug using pre_save signals

--- blogapi/posts/models.py
+++ blogapi/posts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 
 User = get_user_model()
@@ -17,3 +19,9 @@ class Post(models.Model):
     thumbnail = models.ImageField(upload_to=upload_post_image)
     def __str__(self):
         return self.title
+
+def pre_save_receiver(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(pre_save_receiver, sender=Post)
