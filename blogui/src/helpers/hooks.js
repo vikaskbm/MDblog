7717f72b3256c617f0a45e3bab8cd87d13e41d85
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-03 15:19:50 +0530

    Add custom hook to fetch data

---
+++ blogui/src/helpers/hooks.js
@@ -0,0 +1,31 @@
+import {useState, useEffect } from "react"
+import axios from "axios"
+
+function useFetch(url, initialState, ) {
+    const [data, setData] = useState(initialState)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    useEffect(() => {
+        setLoading(true) 
+        async function fetchData() {
+            setLoading(true)
+            try {
+                const res = await axios.get(url) 
+                setData(res.data)
+                setLoading(false)
+            } catch(err) {
+                setError(err.message)
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
+    return {
+        data,
+        error,
+        loading
+    }
+}
+
+export {useFetch}
