e364c68c484d022d288fc82f7b84d8c982287338
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-03 13:26:30 +0530

    Make api call to create post

--- blogui/src/container/PostCreate.js
+++ blogui/src/container/PostCreate.js
@@ -1,19 +1,53 @@
 import React, { useRef, useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 import { Header } from 'semantic-ui-react'
+import axios from 'axios'
+
 import Message from '../components/Message'
+import Loader from '../components/Loader'
+
 
 const PostCreate = () => {
     const [title, setTitle] = useState('')
     const [markdown, setMarkdown] = useState('')
     const [thumbnail, setThumbnail] = useState('')
+    const [loading, setLoading] = useState(null)
+    const [error, setError] = useState(null)
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setLoading(true)
+        console.log(title)
+        console.log(markdown)
+        console.log(thumbnail)
+
+        const formData = new FormData()
+        formData.append("title", title)
+        formData.append("markdown", markdown)
+        formData.append("thumbnail", thumbnail)
+        console.log(formData)
+        axios
+            .post('http://127.0.0.1/8000/api/posts/create/', formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }).then(res => {
+                console.log(res)
+                setLoading(false)
+
+            }).catch(err => {
+                setLoading(false)
+                setError(err.message)
+            })
+    }
     const fileInputRef = useRef()
     return (
         <div>
             <Header>Create a post</Header>
+            {loading && <Loader />}
+            {error && <Message negative message={error}/>}
             {thumbnail && <Message info message={`Selected Image: ${thumbnail.name}`}/>}
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Field>
                     <label>Title</label>
                     <input 
@@ -35,6 +69,7 @@ const PostCreate = () => {
                         content='Choose a thumbnail' 
                         labelPosition='left' 
                         icon='file'
+                        type='button'
                         onClick={ () => fileInputRef.current.click() }    
                     />
                     <input 
