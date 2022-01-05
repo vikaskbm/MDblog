c3efcbed8b0355ba9acb97e20b9b08e0c8f64dde
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-06 05:14:19 +0530

    Create a custom instance of axios with header

---
+++ blogui/src/services/authentication.service.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const authAxios = axios.create()
+
+authAxios.interceptors.request.use(config => {
+    const newConfig = config;
+    const token = localStorage.getItem("token")
+    newConfig.headers = {
+        "Authorization": `Token ${token}`
+    }
+})
