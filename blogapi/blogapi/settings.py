e0cd23429d927207fe0a7fc8f64b44539ea2ce89
Vikas Bishnoi <vikaskbm@gmail.com>
2022-01-06 19:17:46 +0530

    add procfile for project

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -71,11 +71,14 @@ WSGI_APPLICATION = 'blogapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
